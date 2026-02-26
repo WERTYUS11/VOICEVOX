736c2317ad220fe777c8e65298e5dabbdaf493af
WERTYUS11 <116898969+WERTYUS11@users.noreply.github.com>
2026-02-26 19:30:16 +0800

    对部分函数的修改

--- tests/unit/store/singing.spec.ts
+++ tests/unit/store/singing.spec.ts
@@ -7,12 +7,12 @@ import { createDefaultTrack } from "@/sing/domain";
 
 const initialState = cloneWithUnwrapProxy(store.state);
 beforeEach(() => {
-  store.replaceState(initialState);
+  store.replaceState(cloneWithUnwrapProxy(initialState));
 
   resetMockMode();
 });
 
-test("INSERT_TRACK", () => {
+test("插入轨道", () => {
   const dummyTrack = createDefaultTrack();
 
   // 最後尾に追加
